4777432586e94998f88f1358ff04392791596d48
resister-boy <hassanpumped17@gmail.com>
2024-06-01 11:39:58 +0900

    types: add routes types

--- src/types/routes/index.ts
+++ src/types/routes/index.ts
@@ -1,11 +1,15 @@
 import { ROUTES } from '@/src/constant/routes';
 import { StackCodeParams } from '@/src/types';
+import { SignInStepType } from '@/src/types/app';
 
 export type StackParamList = {
   // Screen
   [ROUTES.SCREEN.HOME]: undefined;
   [ROUTES.SCREEN.LOADING]: StackCodeParams;
   [ROUTES.SCREEN.ERROR]: StackCodeParams;
+  [ROUTES.SCREEN.SIGN_IN]: StackCodeParams & {
+    step: SignInStepType;
+  };
 
   // Modal
   [ROUTES.MODAL.ERROR]: StackCodeParams;
