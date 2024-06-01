93d0501ba81097aad09989f7e1989df067110f25
resister-boy <hassanpumped17@gmail.com>
2024-06-01 11:39:06 +0900

    config: add packages

--- metro.config.js
+++ metro.config.js
@@ -1,11 +1,33 @@
 const {getDefaultConfig, mergeConfig} = require('@react-native/metro-config');
 
+const defaultConfig = getDefaultConfig(__dirname);
+const { assetExts, sourceExts } = defaultConfig.resolver;
+
 /**
  * Metro configuration
  * https://facebook.github.io/metro/docs/configuration
  *
  * @type {import('metro-config').MetroConfig}
  */
-const config = {};
 
-module.exports = mergeConfig(getDefaultConfig(__dirname), config);
+const svgConfig = {
+  transformer: {
+    babelTransformerPath: require.resolve("react-native-svg-transformer")
+  },
+  resolver: {
+    assetExts: assetExts.filter((ext) => ext !== "svg"),
+    sourceExts: [...sourceExts, "svg"]
+  }
+};
+
+const styleConfig = {
+  transformer: {
+    babelTransformerPath: require.resolve("react-native-sass-transformer")
+  },
+  resolver: {
+    sourceExts: [...sourceExts, "scss", "sass"]
+  }
+};
+
+
+module.exports = mergeConfig(defaultConfig, [styleConfig, svgConfig]);
